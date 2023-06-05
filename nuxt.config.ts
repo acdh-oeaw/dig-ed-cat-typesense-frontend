fd6bb89fba2197b32c80046cad726598dfc738b3
Robin Kaggl <robin.kaggl@acdh.oeaw.ac.at>
2023-06-05 17:52:15 +0200

    trying to auto deploy to gh pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,23 @@
 import { fileURLToPath } from "node:url";
 
 export default defineNuxtConfig({
-	alias: {
-		"@": fileURLToPath(new URL("./src", import.meta.url)),
-		"~": fileURLToPath(new URL("./", import.meta.url)),
-	},
-	css: ["tailwindcss/tailwind.css"],
-	imports: {
-		autoImport: false,
-	},
-	srcDir: "./src/",
-	postcss: {
-		plugins: {
-			"tailwindcss/nesting": "postcss-nesting",
-			tailwindcss: {},
-			autoprefixer: {},
-		},
-	},
+  alias: {
+    "@": fileURLToPath(new URL("./src", import.meta.url)),
+    "~": fileURLToPath(new URL("./", import.meta.url)),
+  },
+  app: {
+    baseURL: "/dig-ed-cat-typesense-frontend/",
+  },
+  css: ["tailwindcss/tailwind.css"],
+  imports: {
+    autoImport: false,
+  },
+  srcDir: "./src/",
+  postcss: {
+    plugins: {
+      "tailwindcss/nesting": "postcss-nesting",
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
