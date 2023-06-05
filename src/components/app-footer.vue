296e06f91e5708c721366d0b50f5ca3f50240ab8
Robin Kaggl <robin.kaggl@acdh.oeaw.ac.at>
2023-06-05 18:02:34 +0200

    removed link to non existent logo

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,17 +1,14 @@
 <template>
-	<footer>
-		<div
-			class="mx-auto grid w-full max-w-[20rem] gap-8 px-8 py-4 text-xs sm:max-w-container sm:grid-cols-7 sm:text-sm"
-		>
-			<div>
-				<img src="@/assets/images/logo-acdh-with-text.svg" alt="" />
-			</div>
-			<div>
-				<img src="@/assets/images/logo-oeaw.svg" alt="" />
-			</div>
-			<div>
-				<img src="@/assets/images/logo-fwf.svg" alt="" />
-			</div>
-		</div>
-	</footer>
+  <footer>
+    <div
+      class="mx-auto grid w-full max-w-[20rem] gap-8 px-8 py-4 text-xs sm:max-w-container sm:grid-cols-7 sm:text-sm"
+    >
+      <div>
+        <img src="@/assets/images/logo-acdh-with-text.svg" alt="" />
+      </div>
+      <div>
+        <img src="@/assets/images/logo-oeaw.svg" alt="" />
+      </div>
+    </div>
+  </footer>
 </template>
