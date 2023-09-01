079640e7e2f784e3eeb1901e30161eff599b6553
kaggl <robin.kaggl@oeaw.ac.at>
2023-09-01 16:07:16 +0200

    remade old footer, resolves #3

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,14 +1,77 @@
 <template>
-	<footer class="w-screen min-w-full shadow z-10">
-		<div
-			class="grid mx-auto sm:grid-cols-2 gap-8 px-8 py-4 text-xs sm:max-w-container sm:text-sm"
-			:class="$route.path != '/' && 'grayscale'"
-		>
-			<div>
-				<img src="@/assets/images/logo-acdh-with-text.svg" alt="" class="max-h-16 mx-auto" />
+	<footer class="shadow bg-slate-100">
+		<div class="container mx-auto p-2">
+			<div class="grid grid-cols-1 sm:grid-cols-2 gap-4 my-4 justify-around">
+				<div>
+					<a href="https://www.oeaw.ac.at/acdh/" class="navlink content-center">
+						<img
+							src="@/assets/images/logo-acdh-with-text.svg"
+							class="h-16 w-auto mx-auto"
+							alt="ACDHCH"
+							title="Austrian Centre for Digital Humanities and Cultural Heritage"
+						/>
+					</a>
+				</div>
+				<div>
+					<a href="http://www.ucl.ac.uk/dh/" class="navlink">
+						<img
+							src="@/assets/images/logo-ucldh.png"
+							class="h-16 w-auto mx-auto"
+							alt="UCLDH"
+							title="UCL Centre for Digital Humanities"
+						/>
+					</a>
+				</div>
 			</div>
-			<div>
-				<img src="@/assets/images/logo-ucldh.png" alt="" class="max-h-16 mx-auto" />
+			<hr />
+			<div class="my-2 font-bold">Contact</div>
+			<div class="my-2">
+				Greta Franzini
+				<br />
+				g.franzini.11@ucl.ac.uk
+			</div>
+			<div class="my-2 font-bold">Find more on the web</div>
+			<div class="flex my-2">
+				<a
+					href="https://github.com/acdh-oeaw/dig-ed-cat-typesense-frontend"
+					class="flex w-20 h-20 rounded-full bg-slate-600 m-1 items-center justify-around p-4"
+				>
+					<img src="@/assets/images/logo-github.png" alt="Github" title="Github" />
+				</a>
+				<a
+					href="https://twitter.com/search?q=%23digEdsCat"
+					class="flex w-20 h-20 rounded-full bg-slate-600 m-1 items-center justify-around p-4"
+				>
+					<img src="@/assets/images/logo-twitter.png" alt="Twitter" title="DigEdsCat on Twitter" />
+				</a>
+				<a
+					href="https://www.researchgate.net/project/The-Digital-Editions-Catalogue-App-A-web-application-to-browse-curate-and-analyse-digital-editions"
+					class="flex w-20 h-20 rounded-full bg-slate-600 m-1 items-center justify-around p-4"
+				>
+					<img
+						src="@/assets/images/logo-researchgate.png"
+						alt="Research gate"
+						title="Research gate"
+					/>
+				</a>
+				<a
+					href="https://www.openaire.eu/search/dataset?datasetId=r37b0ad08687::c2ad7b8b5225d5abc748a1c6bbb07aeb"
+					class="flex w-20 h-20 rounded-full bg-slate-600 m-1 items-center justify-around p-4"
+				>
+					<img src="@/assets/images/logo-openaire.png" alt="Open Aire" title="Open Aire" />
+				</a>
+				<a
+					href="https://www.zotero.org/groups/2199751/digital_scholarly_editions"
+					class="flex w-20 h-20 rounded-full bg-slate-600 m-1 items-center justify-around p-4"
+				>
+					<img src="@/assets/images/logo-zotero.png" alt="Zotero" title="Zotero" />
+				</a>
+				<a
+					href="https://creativecommons.org/licenses/by/4.0/"
+					class="flex w-20 h-20 rounded-full bg-slate-600 m-1 items-center justify-around p-4"
+				>
+					<img src="@/assets/images/logo-cc.png" alt="CC-BY-SA-4.0" title="CC-BY-SA-4.0 license" />
+				</a>
 			</div>
 		</div>
 	</footer>
