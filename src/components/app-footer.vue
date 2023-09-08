d790e14a105f6053d6f5aa4acb056a4bce748222
kaggl <robin.kaggl@oeaw.ac.at>
2023-09-08 11:38:58 +0200

    layout fixing

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,28 +1,7 @@
 <template>
 	<footer class="shadow bg-slate-100">
 		<div class="container mx-auto p-2">
-			<div class="flex flex-wrap gap-4 my-4 justify-around">
-				<div>
-					<a href="https://www.oeaw.ac.at/acdh/" class="navlink content-center">
-						<img
-							src="@/assets/images/logo-acdh-with-text.svg"
-							class="h-16 w-auto mx-auto"
-							alt="ACDHCH"
-							title="Austrian Centre for Digital Humanities and Cultural Heritage"
-						/>
-					</a>
-				</div>
-				<div>
-					<a href="http://www.ucl.ac.uk/dh/" class="navlink">
-						<img
-							src="@/assets/images/logo-ucldh.png"
-							class="h-16 w-auto mx-auto"
-							alt="UCLDH"
-							title="UCL Centre for Digital Humanities"
-						/>
-					</a>
-				</div>
-			</div>
+			<FooterLogos />
 			<hr />
 			<div class="my-2 font-bold">
 				<nuxt-link href="/imprint">Imprint</nuxt-link>
