b6e557eb321eb2e1b3ba01cdd6e259bbdd26636e
Robin Kaggl <48437174+kaggl@users.noreply.github.com>
2023-09-12 13:47:21 +0200

    attempting to fix facet counts (#14)
    Search terms now affect facet counts, but not other facets. I will come
    back to this at a later point, #11
    
    Co-authored-by: kaggl <robin.kaggl@oeaw.ac.at>

--- src/composable/use-data.ts
+++ src/composable/use-data.ts
@@ -1,17 +1,16 @@
 import { useDefaultClient, useAsyncData } from "#imports";
 import { log } from "console";
 import {
-  type SearchParams,
-  type SearchParamsWithPreset,
-  type SearchResponse,
+	type SearchParams,
+	type SearchParamsWithPreset,
+	type SearchResponse,
 } from "typesense/lib/Typesense/Documents";
 import { type Ref } from "vue";
+import type { LocationQuery } from "vue-router";
 
-export async function getDocument<CollectionEntry extends Record<string, any>>(
-  id: string
-) {
-  // not working on reload as of now
-  /*
+export async function getDocument<CollectionEntry extends Record<string, any>>(id: string) {
+	// not working on reload as of now
+	/*
   const { data, error } = await useAsyncData("edition", () =>
     useDefaultClient()
       .collections<CollectionEntry>("dig-ed-cat")
@@ -23,41 +22,37 @@ export async function getDocument<CollectionEntry extends Record<string, any>>(
   return data;
   */
 
-  return useDefaultClient()
-    .collections<CollectionEntry>("dig-ed-cat")
-    .documents(id)
-    .retrieve();
+	return useDefaultClient().collections<CollectionEntry>("dig-ed-cat").documents(id).retrieve();
 }
 
 export async function getDocuments<CollectionEntry extends Record<string, any>>(
-  query: SearchParams | SearchParamsWithPreset,
-  collection: string = "editions"
+	query: SearchParams | SearchParamsWithPreset,
+	collection: string = "editions",
 ) {
-  // const { data, error } = await useAsyncData(collection, () =>
-  return useDefaultClient()
-    .collections<CollectionEntry>("dig-ed-cat")
-    .documents()
-    .search(query);
-  // );
+	// const { data, error } = await useAsyncData(collection, () =>
+	return useDefaultClient().collections<CollectionEntry>("dig-ed-cat").documents().search(query);
+	// );
 
-  // if (error) console.error(error);
-  // return data;
+	// if (error) console.error(error);
+	// return data;
 }
 
 export async function getFacets<CollectionEntry extends Record<string, any>>(
-  facets: string,
-  max: number = 500,
-  facetQuery: string = ""
+	facets: string,
+	max: number = 500,
+	query: LocationQuery = {},
+	facetQuery: string = "",
 ) {
-  return useDefaultClient()
-    .collections<CollectionEntry>("dig-ed-cat")
-    .documents()
-    .search({
-      q: "*",
-      query_by: "edition-name",
-      per_page: 0,
-      facet_by: facets,
-      facet_query: `${facets}:${facetQuery}`,
-      max_facet_values: max,
-    });
+	return useDefaultClient()
+		.collections<CollectionEntry>("dig-ed-cat")
+		.documents()
+		.search({
+			q: "*",
+			per_page: 0,
+			query_by: "edition-name",
+			...query,
+			facet_by: facets,
+			facet_query: `${facets}:${facetQuery}`,
+			max_facet_values: max,
+		});
 }
