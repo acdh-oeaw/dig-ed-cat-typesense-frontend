e6671c4decd8ef943f555d1d0ffd2f28edf9688d
kaggl <48437174+kaggl@users.noreply.github.com>
2023-09-26 12:01:47 +0000

    Deploying to gh-pages from @ acdh-oeaw/dig-ed-cat-typesense-frontend@600adf32552afeae0af0b412be8adebebf9e8681 🚀

---
+++ _nuxt/centered.8385efa0.js
@@ -0,0 +1,1 @@
+import{_ as t,o as c,c as r,k as s}from"./entry.6a8fe295.js";const n={},o={ref:"centeredRef",class:"relative isolate inset-0 grid h-full w-full place-items-center"};function l(e,a){return c(),r("div",o,[s(e.$slots,"default")],512)}const i=t(n,[["render",l]]);export{i as c};
